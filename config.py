37edb7aeb886aaa6404982560b9eca80ed2fbc28
jhnl18 <jhunelpenaflorida@gmail.com>
2025-11-18 17:40:13 +0800

    Update db connection

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/flask_rbac_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'your_secret_key'
